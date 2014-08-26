da9eaa30a3af11dbe1c69f9d73b4124de11c399b
joaquin <joaquin_pega@hotmail.com>
2014-08-25 22:04:38 -0300

    Added a few more logic to Manager

--- simple-model/src/main/java/org/simple/model/Manager.java
+++ simple-model/src/main/java/org/simple/model/Manager.java
@@ -1,5 +1,6 @@
 package org.simple.model;
 
+import java.util.ArrayList;
 import java.util.List;
 /**
  * 
@@ -9,5 +10,25 @@ import java.util.List;
 public class Manager {
 	private String name;
 	private List<User> users;
+	public Manager(String name) {
+		super();
+		this.name = name;
+		this.users = new ArrayList<User>();
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public List<User> getUsers() {
+		return users;
+	}
+	public void setUsers(List<User> users) {
+		this.users = users;
+	}
+	public void addUser(User user){
+		this.users.add(user);
+	}
 	
 }
