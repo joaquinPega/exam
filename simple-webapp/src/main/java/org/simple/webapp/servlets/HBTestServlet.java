401cb7b6f686d6f32ec7589f12e781e590bdf8cf
joaquinPega <joaquin_pega@hotmail.com>
2014-08-26 11:51:13 -0300

    test01

--- simple-webapp/src/main/java/org/simple/webapp/servlets/HBTestServlet.java
+++ simple-webapp/src/main/java/org/simple/webapp/servlets/HBTestServlet.java
@@ -1,12 +1,18 @@
 package org.simple.webapp.servlets;
 
 import java.io.IOException;
+import java.util.ArrayList;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
+import org.simple.dao.impl.UserDaoImpl;
+import org.simple.model.Manager;
+import org.simple.model.User;
+
+/**@author joaquin.pega
  * Servlet implementation class HBTestServlet
  */
 public class HBTestServlet extends HttpServlet {
@@ -24,7 +30,20 @@ public class HBTestServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		User joaquin = new User();
+		Manager kevin = new Manager("kevin");
+		ArrayList<Manager> managers = new ArrayList<Manager>();
+		managers.add(kevin);
+		joaquin.setEmail("joaquin_pega@hotmail.com");
+		joaquin.setName("joaquin");
+		joaquin.setCompany("Globant");
+		joaquin.setAdmin(true);
+		kevin.addUser(joaquin);
+		joaquin.setManagers(managers);
+		UserDaoImpl userDao= new UserDaoImpl();
+		userDao.save(joaquin);
+		response.getWriter().write("magia");
+		
 	}
 
 	/**
