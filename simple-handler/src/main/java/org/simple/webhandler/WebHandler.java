af5c21832322731d6cd527cb9804871e242de172
joaquinPega <joaquin_pega@hotmail.com>
2014-08-28 12:57:56 -0300

    simple-handler WebHandler con busqueda por pagina de una variable predefinida

--- simple-handler/src/main/java/org/simple/webhandler/WebHandler.java
+++ simple-handler/src/main/java/org/simple/webhandler/WebHandler.java
@@ -13,7 +13,7 @@ import org.simple.model.User;
 import org.simple.persistence.UserDAO;
 
 public class WebHandler {
-
+	private final int MAX_USERS_PER_PAGE =20;
 	public String startAplication() {
 		// TODO: add web start security here initialize all the req for the
 		// aplication
@@ -24,11 +24,16 @@ public class WebHandler {
 		// Todo: get 20 users by page searched
 		List<User> users =null;
 		users =new UserDAO().getAll();
-		return users;
+		if(users.size()>(MAX_USERS_PER_PAGE*page)){
+			return users.subList((page*MAX_USERS_PER_PAGE)-MAX_USERS_PER_PAGE, 20*MAX_USERS_PER_PAGE);
+		}else{
+			return users;
+		}
+		
 	}
 	
 	public String deleteUser(User user){
-		String result="aError";
+		String result="Error";
 		Thread deleteU = new Thread(new DeleteUser(user));
 		try{
 			deleteU.start();
