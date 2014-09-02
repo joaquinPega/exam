d04c2103722d73437d3cc047def356e5a14c6393
Artiom Amerhanov <artiom.amerhanov@globant.com>
2014-09-02 15:56:30 -0300

    tests en DAOs

--- simple-persistence/src/test/java/org/simple/persistence/TestManagerDAO.java
+++ simple-persistence/src/test/java/org/simple/persistence/TestManagerDAO.java
@@ -9,27 +9,46 @@ import junit.framework.TestCase;
 public class TestManagerDAO extends TestCase {
 
 	public void testSave() {
-		ManagerDAO manager = new ManagerDAO();
-		
-		
+		ManagerDAO md = new ManagerDAO();
+		Manager mn = new Manager();
+		mn.setName("pepito");
+		md.save(mn);
+
 	}
 
 	public void testUpdate() {
+		ManagerDAO md = new ManagerDAO();
+		List<Manager> m = md.getAll();
+		Manager manager = m.get(0);
+		manager.setName("artiom");
+		md.update(manager);
 	}
 
 	public void testDelete() {
+		ManagerDAO md = new ManagerDAO();
+		List<Manager> m = md.getAll();
+		for (Manager m2 : m) {
+			if (m2.getName().equals("pepito")) {
+				md.delete(m2);
+				break;
+			}
+		}
 	}
 
 	public void testSearch() {
 		ManagerDAO md = new ManagerDAO();
 		List<Manager> m = md.search("Manager1");
-		for(Manager m1 : m){
+		for (Manager m1 : m) {
 			System.out.println(m1.getName());
 		}
-		
+
 	}
 
 	public void testGetAll() {
+		ManagerDAO md = new ManagerDAO();
+		List<Manager> lista;
+		lista = md.getAll();
+
 	}
 
 }
