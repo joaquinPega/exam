69733bd5f361f04b61b33d6cd62bdb142e0f8e7a
joaquinPega <joaquin_pega@hotmail.com>
2014-09-02 17:37:46 -0300

    Mejorada el inicio de la base de datos de pruebas

--- simple-webapp/src/test/java/org/simple/webapp/servlets/InicioServlet.java
+++ simple-webapp/src/test/java/org/simple/webapp/servlets/InicioServlet.java
@@ -1,6 +1,7 @@
 package org.simple.webapp.servlets;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -29,14 +30,35 @@ public class InicioServlet extends HttpServlet {
 			throws ServletException, IOException {
 		WebHandler wh = WebHandler.getInstance();
 		try {
-			for (int i = 0; i < 100; i++) {
+			String[] m1=new String[9];
+			m1[0]="Kevin";
+			m1[1]="Artiom";
+			m1[2]="Lautaro";
+			m1[3]="Nicolas";
+			ArrayList<Manager> managers = new ArrayList<Manager>();
+			ArrayList<Language> languages = new ArrayList<Language>();
+			for(int i=0;i<4;i++){
 				Manager m = new Manager();
+				m.setName(m1[i]);
+				wh.saveManager(m);
+				managers.add(m);			
+			}
+			m1[0]="Ingles";
+			m1[1]="Español";
+			m1[2]="Japones";
+			m1[3]="Chino";
+			m1[4]="Coreano";
+			m1[5]="Frances";
+			for(int i = 0;i<6;i++){
 				Language l = new Language();
+				l.setName(m1[i]);
+				wh.saveLanguage(l);
+				languages.add(l);
+			}
+			for (int i = 0; i < 100; i++) {
 				User u = new User();
-				m.setName("Manager" + i);
-				l.setName("Language" + i);
-				u.setManager(m);
-				u.setLanguage(l);
+				u.setManager(managers.get(i % 4));
+				u.setLanguage(languages.get(i% 6));
 				u.setName("Name"+i);
 				u.setAdmin(false);
 				u.setCompany("Company"+i);
@@ -44,8 +66,6 @@ public class InicioServlet extends HttpServlet {
 				u.setExperience("Nothing");
 				u.setJobTitle("Jave developer");
 				u.setPassword("asd");
-				wh.saveLanguage(l);
-				wh.saveManager(m);
 				wh.saveUser(u);
 			}
 		} catch (CouldNotFinishOperationException e) {
